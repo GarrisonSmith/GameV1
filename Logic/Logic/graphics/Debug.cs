fe7eb169dee306cb4d9d0cc273c9df4b4fd78ba8
Garrison Smith <44329882+GarrisonSmith@users.noreply.github.com>
2022-05-14 17:42:29 -0700

    Add files via upload

---
+++ Logic/Logic/graphics/Debug.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Fantasy.Content.Logic.screen;
+
+namespace Fantasy.Content.Logic.graphics
+{
+    static class Debug
+    {
+        public static void DebugAll(Scene _scene) {
+           /*
+            _scene._spriteBatch.Begin(SpriteSortMode.BackToFront,
+                        BlendState.AlphaBlend,
+                        null,
+                        null,
+                        null,
+                        null,
+                        _scene._camera.GetTransformation(_scene._graphics.GraphicsDevice));
+           */
+            DrawAxis(_scene);
+            DrawRectangle(_scene, _scene._tileMap.GetTileMapBounding(_scene._camera.zoom));
+            DrawPoint(_scene, _scene._tileMap.GetTileMapCenter(_scene._camera.zoom));
+
+           // _scene._spriteBatch.End();
+        }
+       
+        public static void DrawAxis(Scene _scene)
+        {
+            for (int i = 0; i <= _scene._tileMap.GetTileMapBounding(_scene._camera.zoom).Width + (64 * _scene._camera.zoom.X); i++)
+            {
+                _scene._spriteBatch.Draw(_scene._tileTextures[0],
+                        new Vector2(i, 0),
+                        new Rectangle(0, 0, 1, 1), Color.White, 0, new Vector2(0, 0),
+                        new Vector2(1, 1), new SpriteEffects(), 1);
+            }
+            for (int i = 0; i <= _scene._tileMap.GetTileMapBounding(_scene._camera.zoom).Height + +(64 * _scene._camera.zoom.Y); i++)
+            {
+                _scene._spriteBatch.Draw(_scene._tileTextures[0],
+                        new Vector2(0, -i),
+                        new Rectangle(0, 0, 1, 1), Color.White, 0, new Vector2(0, 0),
+                        new Vector2(1, 1), new SpriteEffects(), 1);
+            }
+        }
+
+        public static void DrawRectangle(Scene _scene, Rectangle foo)
+        {
+            //draws bottom line
+            for (int i = foo.X; i < foo.X + foo.Width; i++)
+            {
+                _scene._spriteBatch.Draw(_scene._tileTextures[0],
+                        new Vector2(i, -foo.Y),
+                        new Rectangle(0, 0, 1, 1), Color.White, 0, new Vector2(0, 0),
+                        new Vector2(1, 1), new SpriteEffects(), 1);
+            }
+            //draws left line
+            for (int i = foo.Y; i < foo.Y + foo.Height; i++)
+            {
+                _scene._spriteBatch.Draw(_scene._tileTextures[0],
+                        new Vector2(foo.X, -i),
+                        new Rectangle(0, 0, 1, 1), Color.White, 0, new Vector2(0, 0),
+                        new Vector2(1, 1), new SpriteEffects(), 1);
+            }
+            //draws top line
+            for (int i = foo.X; i < foo.X + foo.Width; i++)
+            {
+                _scene._spriteBatch.Draw(_scene._tileTextures[0],
+                        new Vector2(i, -foo.Height - (64 * _scene._camera.zoom.X)),
+                        new Rectangle(0, 0, 1, 1), Color.White, 0, new Vector2(0, 0),
+                        new Vector2(1, 1), new SpriteEffects(), 1);
+            }
+            //draws right line
+            for (int i = foo.Y; i < foo.Y + foo.Height; i++)
+            {
+                _scene._spriteBatch.Draw(_scene._tileTextures[0],
+                        new Vector2(foo.Width + (64 * _scene._camera.zoom.Y), -i),
+                        new Rectangle(0, 0, 1, 1), Color.White, 0, new Vector2(0, 0),
+                        new Vector2(1, 1), new SpriteEffects(), 1);
+            }
+        }
+
+        public static void DrawPoint(Scene _scene, Point foo)
+        {
+            _scene._spriteBatch.Draw(_scene._tileTextures[0],
+                        new Vector2(foo.X, -foo.Y),
+                        new Rectangle(0, 0, 2, 2), Color.White, 0, new Vector2(0, 0),
+                        new Vector2(1, 1), new SpriteEffects(), 1);
+        }
+    }
+}
---
+++ Logic/Logic/graphics/TileMapLayer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Fantasy.Content.Logic.graphics
+{
+    /// <summary>
+    /// Desribes a layer of tiles in a given <c>TileMap</c>.
+    /// </summary>
+    class TileMapLayer
+    {
+        /// <summary>
+        /// List containing this layers tiles.
+        /// </summary>
+        public List<Tile> map;
+        /// <summary>
+        /// The layer this TileMapLayer will be drawn on.
+        /// </summary>
+        public int layer;
+        /// <summary>
+        /// The width of this TileMapLayer.
+        /// </summary>
+        public int width;
+        /// <summary>
+        /// The height of this TileMapLayer.
+        /// </summary>
+        public int height;
+
+        /// <summary>
+        /// Constructs a TileMapLayer with the given properties.
+        /// <param name="initialized"> is parsed to describe the tiles in the map. </param>
+        /// </summary>>
+        public TileMapLayer(int layer, String initialize)
+        {
+            this.map = new List<Tile>();
+            this.layer = layer;
+            string[] columnTemp;
+            string[] rowTemp;
+            int row = 2;
+
+            rowTemp = initialize.Split(";");
+            Array.Reverse(rowTemp);
+            for (int i = 0; i < rowTemp.Length; i++)
+            {
+                int column = 1;
+                columnTemp = rowTemp[i].Split(":");
+                if (rowTemp[i] != "")
+                {
+                    foreach (string j in columnTemp)
+                    {
+                        if (j != "")
+                        {
+                            map.Add(new Tile(j, column, row));
+                            if (column+1 > this.width)
+                            {
+                                this.width = column+1;
+                            }
+                            if (row > this.height)
+                            {
+                                this.height = row;
+                            }
+                        }
+                        column++;
+                    }
+                    
+                    row++;
+                }
+            }
+        }
+        /// <summary>
+        /// Returns the tile with the given index from the TileMaplayer. If the index is invalid returns null.
+        /// </summary>
+        public Tile GetTile(int index)
+        {
+            if (map.Count - 1 >= index)
+            {
+                return map[index];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// Returns the tile with the given coordinate from the TileMaplayer. If the coordiante is invalid returns null.
+        /// </summary>
+        public Tile GetTile(Point coordinate)
+        {
+            foreach (Tile i in map)
+            {
+                if (i.tileMapCoordinate == coordinate)
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// Returns the layer dimensions in as a point containing width and height.
+        /// </summary>
+        public Point GetLayerDimension()
+        {
+            return new Point(this.width, this.height);
+        }
+    }
+}
