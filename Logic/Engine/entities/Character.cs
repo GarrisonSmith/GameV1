e255d3f61ba67ffd864355bdce383e32189b5a15
Garrison Smith <44329882+GarrisonSmith@users.noreply.github.com>
2022-05-18 15:25:33 -0700

    Add files via upload

---
+++ Logic/Engine/entities/Character.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Fantasy.Content.Logic.graphics;
+
+
+namespace Fantasy.Content.Logic.entities
+{
+    class Character : Entity
+    {
+        public bool characterIsMoving;
+        public int speed;
+        public Orientation orientation;
+        public Animation frames;
+
+        public Character() { }
+
+        public Character(string id, string spriteSheetName, int layer, Rectangle positionBox, int speed, Orientation orientation)
+        {
+            this.id = id;
+            this.spriteSheetName = spriteSheetName;
+            this.layer = layer;
+            this.positionBox = positionBox;
+            this.speed = speed;
+            this.orientation = orientation;
+
+            spriteSheet = Global._content.Load<Texture2D>("character-sets/"+spriteSheetName);
+        }
+
+        public void DrawCharacter(Vector2 _stretch, SpriteBatch _spriteBatch)
+        {
+            if (frames == null)
+            {
+                switch (orientation)
+                {
+                    case Orientation.up:
+                        frames = new Animation(400, 400, 0, 3, 3, 0, 64, 128, AnimationState.idle);
+                        break;
+                    case Orientation.right:
+                        frames = new Animation(400, 400, 0, 3, 1, 0, 64, 128, AnimationState.idle);
+                        break;
+                    case Orientation.left:
+                        frames = new Animation(400, 400, 0, 3, 2, 0, 64, 128, AnimationState.idle);
+                        break;
+                    case Orientation.down:
+                        frames = new Animation(400, 400, 0, 3, 0, 0, 64, 128, AnimationState.idle);
+                        break;
+                }
+            }
+
+            if (characterIsMoving)
+            {
+                frames.animationState = AnimationState.cycling;
+            }
+            else if (frames.animationState != AnimationState.idle)
+            {
+                frames.animationState = AnimationState.finishing;
+            }
+
+            if (characterIsMoving)
+            {
+                frames.DrawAnimation(new Point(positionBox.X, positionBox.Y), spriteSheet, Color.White, _stretch);
+            }
+            else
+            {
+                frames.DrawAnimation(new Point(positionBox.X, positionBox.Y), spriteSheet, Color.White, _stretch);
+            }
+        }
+
+        public void MoveCharacter(Orientation direction)
+        {
+            characterIsMoving = true;
+            switch (direction)
+            {
+                case Orientation.up:
+                    positionBox.Y += speed;
+                    break;
+                case Orientation.right:
+                    positionBox.X += speed;
+                    break;
+                case Orientation.left:
+                    positionBox.X -= speed;
+                    break;
+                case Orientation.down:
+                    positionBox.Y -= speed;
+                    break;
+            }
+            if (direction != orientation)
+            {
+                frames.ChangeOrientation(direction);
+                orientation = direction;
+            }
+        }
+
+        public void StopCharacter()
+        {
+            characterIsMoving = false;
+        }
+    }
+}
---
+++ Logic/Engine/screen/Scene.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Fantasy.Content.Logic.graphics;
+using Fantasy.Content.Logic.entities;
+
+namespace Fantasy.Content.Logic.screen
+{
+    class Scene
+    {
+        public Camera _camera;
+        public TileMap _tileMap;
+        public Character _character;
+        public Scene(TileMap _tileMap)
+        {
+            this._tileMap = _tileMap;
+            this._character = new Character("character_two", "character_two_spritesheet", 1, new Rectangle(0,0,64,128), 3, Orientation.up);
+
+            _camera = new Camera(this, new Point(640, 640), true, false);
+        }
+        public void LoadScene()
+        {
+            _tileMap.LoadTileTextures();
+        }
+        public void DrawScene()
+        {
+            Global._spriteBatch.Begin(SpriteSortMode.Deferred, //first things drawn on bottom, last things on top
+                BlendState.AlphaBlend,
+                null,
+                null,
+                null,
+                null,
+                _camera.GetTransformation());
+
+            _tileMap.DrawArea(_camera._stretch, 1, _camera.cameraPosition);
+            Debug.DebugOnScene(this);
+            _character.DrawCharacter(_camera._stretch, Global._spriteBatch);
+
+            Global._spriteBatch.End();
+
+            Global._spriteBatch.Begin();
+
+            Debug.DebugOverlay(this);
+
+            Global._spriteBatch.End();
+        }
+        public void ClearAndRedraw()
+        {
+            Global._graphics.GraphicsDevice.Clear(Color.Gray);
+            Global._graphics.BeginDraw();
+            DrawScene();
+            Global._graphics.EndDraw();
+        }
+        public void TransitionScene(String tileMapString)
+        {
+            _tileMap.UnloadTileTextures();
+            _tileMap = new TileMap(tileMapString);
+            _tileMap.LoadTileTextures();
+            _camera.SetBoundingBox(true);
+            ClearAndRedraw();
+        }
+        public void BufferTest()
+        {
+            Global._graphics.PreferredBackBufferWidth = 500;
+            Global._graphics.PreferredBackBufferHeight = 500;
+            Global._graphics.ApplyChanges();
+            this._camera = new Camera(this, this._camera.cameraCenter, true, true);
+        }
+    }
+}
