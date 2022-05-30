93a6b8d9f339f12638a0c2829174d568c5355473
Garrison Smith <44329882+GarrisonSmith@users.noreply.github.com>
2022-05-30 11:45:16 -0700

    Add files via upload

--- Logic/Engine/hitboxes/Entitybox.cs
+++ Logic/Engine/hitboxes/Entitybox.cs
@@ -1,25 +1,50 @@
 ﻿using Microsoft.Xna.Framework;
 using Fantasy.Logic.Engine.graphics;
-using Fantasy.Logic.Engine.screen;
 
 namespace Fantasy.Logic.Engine.hitboxes
 {
+    /// <summary>
+    /// Hitbox used by Entities to describe their collision area and location.
+    /// </summary>
     class Entitybox : Hitbox
     {
+        /// <summary>
+        /// The entity id this Entitybox refers to.
+        /// </summary>
         public string reference;
+        /// <summary>
+        /// Describes the location, width, and height of the entity this Entitybox refers to.
+        /// </summary>
         public Rectangle characterArea;
 
+        /// <summary>
+        /// Creates a Entitybox with the provided reference and characterArea.
+        /// </summary>
+        /// <param name="reference">The entity id this Entitybox will refer to.</param>
+        /// <param name="characterArea">The location, width, and height of the entity this Entitybox will refer to.</param>
         public Entitybox(string reference, Rectangle characterArea)
         {
             this.reference = reference;
             this.characterArea = characterArea;
         }
-
+        /// <summary>
+        /// Creates a Entitybox with the provided reference, characterArea, and collisionArea.
+        /// </summary>
+        /// <param name="reference">The entity id this Entitybox will refer to.</param>
+        /// <param name="characterArea">The location, width, and height of the entity this Entitybox will refer to.</param>
+        /// <param name="collisionArea">Array of Rectangles that will describe the Tileboxes area.</param>
         public Entitybox(string reference, Rectangle characterArea, Rectangle[] collisionArea) : this(reference, characterArea)
         {
             this.collisionArea = collisionArea;
         }
-
+        /// <summary>
+        /// Checks if the provided newCharacterArea collides with any Tileboxes or Eventboxes in the Global current scenes Tilemap.
+        /// If newCharacterArea is valid (does not result in collision), then newCharacterArea will become this Entityboxes CharacterArea.
+        /// </summary>
+        /// <param name="layer">The layer of the Tliemap to be checked.</param>
+        /// <param name="newCharacterArea">The position, length, and width of this entity that will be checked for collision.</param>
+        /// <returns>True if newCharacterArea is valid (does not result in collision) and the movement is done,
+        /// False if newCharacterArea is not valid (does result in collision) and the movement is not done.</returns>
         public bool AttemptMovement(int layer, Rectangle newCharacterArea)
         {
             Rectangle temp = characterArea;
@@ -31,7 +56,10 @@ namespace Fantasy.Logic.Engine.hitboxes
             }
             return true;
         }
-
+        /// <summary>
+        /// Draws all of the rectangles inside of this boxes collisionArea.
+        /// Used for debugging.
+        /// </summary>
         new public void DrawHitbox()
         {
             foreach (Rectangle foo in collisionArea)
