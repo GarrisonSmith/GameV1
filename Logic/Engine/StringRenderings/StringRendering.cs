7da8bbfda07f40d9f1b9a6478462f5495cdf9b17
Garrison Smith <44329882+GarrisonSmith@users.noreply.github.com>
2022-08-08 21:33:47 -0700

    Add files via upload

---
+++ Logic/Engine/StringRenderings/StringRendering.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Fantasy.Logic.Engine.StringRenderings
+{
+    //a character is 9 pixels wide 16 pixels tall.
+
+
+    public static class StringRendering
+    {
+        public static string formatString(string foo, Rectangle area)
+        {
+            return null;
+        }
+    }
+}
